55942681d9a6046c3277cf9fe50d3ccd04d2d4f7
gconnell-design <39175129+gconnell-design@users.noreply.github.com>
2019-03-28 00:50:07 +1100

    Home: 27/3

---
+++ Assets/Scripts/Hurt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurt : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "quad")
+        {
+            Debug.Log("test:" + gameObject.name);
+        }
+    }
+}
